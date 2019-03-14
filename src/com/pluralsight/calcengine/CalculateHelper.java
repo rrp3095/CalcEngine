7f20a00b41f6f869de25ce51b238568e1860b397
rrp3095 <rrp3095@outlook.com>
2019-03-14 18:54:35 +0530

    Error handling demo: handling error: (2. add xx 1.0) throwing exception for non numeric data

--- src/com/pluralsight/calcengine/CalculateHelper.java
+++ src/com/pluralsight/calcengine/CalculateHelper.java
@@ -20,9 +20,13 @@ public class CalculateHelper {
             this new InvalidStatementException("Incorrect number of fields", statement);
 
         String commandString = parts[0];
-        leftVal = Double.parseDouble(parts[1]);
-        rightVal = Double.parseDouble(parts[2]);
 
+        try {
+            leftVal = Double.parseDouble(parts[1]);
+            rightVal = Double.parseDouble(parts[2]);
+        }catch (NumberFormatException e) {
+            throw new InvalidStatementException("Non numeric data", statement,e);
+        }
         setCommandFromString(commandString);
 
         CalculateBase calculator = null;
